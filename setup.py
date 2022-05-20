35c3c16e0809631c486310f5f1fecfdb06c2ef17
andsfonseca <andsf@live.com>
2022-05-20 16:00:26 -0300

    feat: Up version to v0.1.3

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='NvidiaGPUWaiter',
-    version='0.1.2',    
+    version='0.1.3',    
     description='Method to detect idle GPU with certain conditions using nvidia-smi',
     url='https://github.com/andsfonseca/NvidiaGPUWaiter',
     author='Anderson Silva',
     author_email='andsfonseca@tecgraf.puc-rio.br',
     license='MIT',
-    packages=['NvidiaGPUWaiter'],
+    packages=find_packages(),
     install_requires=[],
     project_urls = {
         "Bug Tracker": "https://github.com/andsfonseca/NvidiaGPUWaiter/issues"
